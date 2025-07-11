61981c9c7fbd632806a4a3e0df6f5670b0289fcf
YadneshNikam <yadneshnikam32@gmail.com>
2025-07-11 10:38:49 +0530

    Add files via upload

---
+++ input_output.cpp
@@ -0,0 +1,13 @@
+//input and output
+#include <iostream>
+using namespace std;
+int main(){
+    int num1,num2,sum;
+    cout<<"Enter num1 value:";
+    cin>>num1;
+    cout<<"Enter num2 value:";
+    cin>>num2;
+    sum=num1+num2;
+    cout<<"sum="<<sum;
+return 0;
+}
